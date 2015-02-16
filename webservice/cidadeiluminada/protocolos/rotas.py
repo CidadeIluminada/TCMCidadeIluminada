59956ff744b092a0853c83e3fd8795700531e529
Arthur Bressan <arthurpbressan@gmail.com>
2015-02-16 14:01:07 -0200

    Adiciona rota para upload de arquivos

--- webservice/cidadeiluminada/protocolos/rotas.py
+++ webservice/cidadeiluminada/protocolos/rotas.py
@@ -1,7 +1,10 @@
 # coding: UTF-8
 from __future__ import absolute_import
+import os
 
-from flask import Blueprint, jsonify
+from flask import Blueprint, jsonify, request, current_app, redirect, url_for,\
+    send_from_directory
+from werkzeug import secure_filename
 
 from cidadeiluminada.protocolos.models import Protocolo
 
@@ -10,6 +13,11 @@ bp = Blueprint('protocolos', __name__)
 
 def init_app(app, url_prefix='/protocolos'):
     app.register_blueprint(bp, url_prefix=url_prefix)
+
+
+def _allowed_file(filename):
+    return '.' in filename and filename.rsplit('.', 1)[1] in \
+        current_app.config['ALLOWED_EXTENSIONS']
 
 
 @bp.route('/')
@@ -23,6 +31,26 @@ def lista():
     return jsonify(payload=protocolos)
 
 
-@bp.route('/novo/', methods=['POST'])
+@bp.route('/novo/', methods=['GET', 'POST'])
 def novo():
-    pass
+    if request.method == 'POST':
+        file_ = request.files['file']
+        if file_ and _allowed_file(file_.filename):
+            filename = secure_filename(file_.filename)
+            file_.save(os.path.join(current_app.config['UPLOAD_FOLDER'],
+                                    filename))
+            return redirect(url_for('.uploaded_file', filename=filename))
+    return '''
+    <!doctype html>
+    <title>Novo protocolo</title>
+    <h1>Novo protocolo</h1>
+    <form action="" method=post enctype=multipart/form-data>
+        <input type=file name=file>
+        <input type=submit value=Upload>
+    </form>
+    '''
+
+
+@bp.route('/files/<filename>/')
+def uploaded_file(filename):
+    return send_from_directory(current_app.config['UPLOAD_FOLDER'], filename)
--- webservice/settings.py
+++ webservice/settings.py
@@ -1,7 +1,12 @@
 #coding=UTF-8
+import os
 
 DEBUG = True
 CSRF_ENABLED = True
 SECRET_KEY = 'dontcare'
 
 SQLALCHEMY_DATABASE_URI = 'sqlite:///test.db'
+
+UPLOAD_FOLDER = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'tmp')
+
+ALLOWED_EXTENSIONS = set(['png', 'jpg'])
