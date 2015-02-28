14e6836beb364c6def4c2d3d0d0960ece8d51d92
Arthur Bressan <arthurpbressan@gmail.com>
2015-02-28 13:58:39 -0300

    Adiciona suporte ao postgres

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 sudo add-apt-repository ppa:webupd8team/java
 sudo apt-get update
-sudo apt-get install -y oracle-java8-installer python-dev git python-pip postgresql postgresql-contrib pgadmin3 vim lib32ncurses5-dev lib32stc++6
+sudo apt-get install -y oracle-java8-installer python-dev git python-pip postgresql postgresql-contrib pgadmin3 vim lib32ncurses5-dev lib32stc++6 postgresql-server-dev-9.3
 sudo pip install --upgrade pip virtualenvwrapper
 sudo mkdir ~/.virtualenvs
 echo 'export WORKON_HOME=$HOME/.virtualenvs' >> ~/.bashrc
