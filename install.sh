180ed7142dd7b6ae518ba8500245440f5e0dde44
Arthur Bressan <arthurpbressan@gmail.com>
2015-03-09 20:28:02 -0300

    Adiciona chromium no install

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 sudo add-apt-repository ppa:webupd8team/java
 sudo apt-get update
-sudo apt-get install -y oracle-java8-installer python-dev git python-pip postgresql postgresql-contrib pgadmin3 vim lib32ncurses5-dev lib32stdc++6 postgresql-server-dev-9.4
+sudo apt-get install -y oracle-java8-installer python-dev git python-pip postgresql postgresql-contrib pgadmin3 vim lib32ncurses5-dev lib32stdc++6 postgresql-server-dev-9.4 chromium-browser
 pip install --upgrade pip virtualenvwrapper
 mkdir ~/.virtualenvs
 echo 'export WORKON_HOME=$HOME/.virtualenvs' >> ~/.bashrc
