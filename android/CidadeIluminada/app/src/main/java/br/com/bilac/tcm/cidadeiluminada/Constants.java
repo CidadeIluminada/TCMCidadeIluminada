3fcf1161b8117e0b68cef33dc35b31d5e9bcf6b1
Arthur Bressan <arthurpbressan@gmail.com>
2015-03-22 18:48:35 -0300

    Adiciona classe de constantes e organiza o código

---
+++ android/CidadeIluminada/app/src/main/java/br/com/bilac/tcm/cidadeiluminada/Constants.java
@@ -0,0 +1,9 @@
+package br.com.bilac.tcm.cidadeiluminada;
+
+/**
+ * Created by arthur on 22/03/15.
+ */
+public final class Constants {
+    public static final int CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE = 100;
+    public static final String APPLICATION_NAME = "CidadeIluminada";
+}
--- android/CidadeIluminada/app/src/main/java/br/com/bilac/tcm/cidadeiluminada/protocolos/ProtocoloActivity.java
+++ android/CidadeIluminada/app/src/main/java/br/com/bilac/tcm/cidadeiluminada/protocolos/ProtocoloActivity.java
@@ -19,25 +19,21 @@ import java.io.File;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
+import br.com.bilac.tcm.cidadeiluminada.Constants;
 import br.com.bilac.tcm.cidadeiluminada.R;
 
 public class ProtocoloActivity extends ActionBarActivity {
+
+    private Uri fileUri;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_protocolo);
-
-        if (fileUri != null) {
-            ImageView img = (ImageView) findViewById(R.id.imageView);
-            Bitmap bmp = decodeSampledBitmapFromFile(fileUri.getPath(), img.getWidth(), img.getHeight());
-            img.setImageBitmap(bmp);
-        }
     }
 
     @Override
     protected void onSaveInstanceState(Bundle outState) {
-        outState.putParcelable("fileUri", fileUri);
         super.onSaveInstanceState(outState);
     }
 
@@ -47,124 +43,72 @@ public class ProtocoloActivity extends ActionBarActivity {
         getMenuInflater().inflate(R.menu.menu_protocolo, menu);
         return true;
     }
-
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
-
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
             return true;
         }
-
         return super.onOptionsItemSelected(item);
     }
 
-    private static final int CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE = 100;
-    private Uri fileUri = null;
     public void abrirCamera(View view) {
         Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-
-        fileUri = getOutputMediaFileUri(MEDIA_TYPE_IMAGE);
-        intent.putExtra(MediaStore.EXTRA_OUTPUT, fileUri); // set the image file name
-
-        // start the image capture Intent
-        startActivityForResult(intent, CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE);
-
+        fileUri = getOutputMediaFileUri();
+        intent.putExtra(MediaStore.EXTRA_OUTPUT, fileUri);
+        startActivityForResult(intent, Constants.CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE);
     }
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        if (requestCode == CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE) {
+        if (requestCode == Constants.CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE) {
             if (resultCode == RESULT_OK) {
-                // Image captured and saved to fileUri specified in the Intent
-                String path = fileUri.getPath();
-                Toast.makeText(this, "Image saved to:\n" + path, Toast.LENGTH_LONG).show();
                 ImageView img = (ImageView) findViewById(R.id.imageView);
-                Bitmap bmp = decodeSampledBitmapFromFile(path, img.getWidth(), img.getHeight());
+                Bitmap bmp = decodeSampledBitmapFromFile(fileUri.getPath(), img.getWidth(),
+                                                         img.getHeight());
                 img.setImageBitmap(bmp);
-            } else {
-                if (resultCode == RESULT_CANCELED) {
-                    Toast.makeText(this, "Cancelado", Toast.LENGTH_SHORT).show();
-                } else {
-                    Toast.makeText(this, "Erro", Toast.LENGTH_LONG).show();
-                }
             }
         }
     }
 
-    public static Bitmap decodeSampledBitmapFromFile(String path, int reqWidth, int reqHeight)
-    { // BEST QUALITY MATCH
-
-        //First decode with inJustDecodeBounds=true to check dimensions
+    private Bitmap decodeSampledBitmapFromFile(String path, int requiredWidth, int requiredHeight) {
         final BitmapFactory.Options options = new BitmapFactory.Options();
         options.inJustDecodeBounds = true;
         BitmapFactory.decodeFile(path, options);
-
-        // Calculate inSampleSize, Raw height and width of image
         final int height = options.outHeight;
         final int width = options.outWidth;
         options.inPreferredConfig = Bitmap.Config.RGB_565;
         int inSampleSize = 1;
-
-        if (height > reqHeight)
-        {
-            inSampleSize = Math.round((float)height / (float)reqHeight);
+        if (height > requiredHeight) {
+            inSampleSize = Math.round((float)height / (float)requiredHeight);
         }
         int expectedWidth = width / inSampleSize;
-
-        if (expectedWidth > reqWidth)
-        {
-            //if(Math.round((float)width / (float)reqWidth) > inSampleSize) // If bigger SampSize..
-            inSampleSize = Math.round((float)width / (float)reqWidth);
+        if (expectedWidth > requiredWidth) {
+            inSampleSize = Math.round((float)width / (float)requiredWidth);
         }
-
         options.inSampleSize = inSampleSize;
-
-        // Decode bitmap with inSampleSize set
         options.inJustDecodeBounds = false;
-
         return BitmapFactory.decodeFile(path, options);
     }
 
-    public static final int MEDIA_TYPE_IMAGE = 1;
-    public static final int MEDIA_TYPE_VIDEO = 2;
-
-    /** Create a file Uri for saving an image or video */
-    private Uri getOutputMediaFileUri(int type){
-        return Uri.fromFile(getOutputMediaFile(type));
-    }
-
-    /** Create a File for saving an image or video */
-    private File getOutputMediaFile(int type){
-        // To be safe, you should check that the SDCard is mounted
-        // using Environment.getExternalStorageState() before doing this.
+    private Uri getOutputMediaFileUri(){
         if (!Environment.getExternalStorageState().equals(Environment.MEDIA_MOUNTED)) {
-            Log.d(this.getClass().getSimpleName(), "not mounted");
             return null;
         }
-        File mediaStorageDir = new File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES), "MyCameraApp");
+        File mediaStorageDir =
+                new File(Environment.getExternalStoragePublicDirectory(
+                         Environment.DIRECTORY_PICTURES), Constants.APPLICATION_NAME);
         if (!mediaStorageDir.exists()){
             if (!mediaStorageDir.mkdirs()){
-                Log.d(this.getClass().getSimpleName(), "failed to create directory");
                 return null;
             }
         }
         String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
-        File mediaFile;
-        if (type == MEDIA_TYPE_IMAGE) {
-            mediaFile = new File(mediaStorageDir.getPath() + File.separator + "IMG_"+ timeStamp + ".jpg");
-        } else if(type == MEDIA_TYPE_VIDEO) {
-            mediaFile = new File(mediaStorageDir.getPath() + File.separator + "VID_"+ timeStamp + ".mp4");
-        } else {
-            return null;
-        }
-
-        Log.d(this.getClass().getName(), mediaFile.getPath());
-
-        return mediaFile;
+        return Uri.fromFile(new File(mediaStorageDir.getPath() +
+                            File.separator + "IMG_"+ timeStamp + ".jpg"));
     }
 }
