18be74f3ae5e7164c2d4634b2ef290e7e78cf08d
Arthur Bressan <arthurpbressan@gmail.com>
2015-03-28 19:54:38 -0300

    Adiciona chave do host do serviço

--- android/CidadeIluminada/app/src/main/java/br/com/bilac/tcm/cidadeiluminada/Constants.java
+++ android/CidadeIluminada/app/src/main/java/br/com/bilac/tcm/cidadeiluminada/Constants.java
@@ -6,4 +6,6 @@ package br.com.bilac.tcm.cidadeiluminada;
 public final class Constants {
     public static final int CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE = 100;
     public static final String APPLICATION_NAME = "CidadeIluminada";
+
+    public static final String CIDADEILUMINADA_HOST = "http://192.168.0.109:5000";
 }
